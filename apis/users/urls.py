eaa36ceb11ecfe5aafe37ea037abb488fb86f31b
Fasih Muhammad Virk <82199241+fasihmuhammadvirk@users.noreply.github.com>
2025-02-03 19:04:50 +0500

    fix issues related to migrations

--- apis/users/urls.py
+++ apis/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apis.users.views import UserListCreateView, UserRetrieveUpdateDeleteView
 
 urlpatterns = [
-    path('api/v1/', UserListCreateView.as_view(), name='user-list-create'),
-    path('api/v1/<int:pk>/', UserRetrieveUpdateDeleteView.as_view(), name='user-detail'),
+    path('api/', UserListCreateView.as_view(), name='user-list-create'),
+    path('api/<int:pk>/', UserRetrieveUpdateDeleteView.as_view(), name='user-detail'),
 ]
