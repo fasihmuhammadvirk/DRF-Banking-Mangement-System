5d68869bb3dadfc69fdc0077a29838bf2c1dd434
Fasih Muhammad Virk <82199241+fasihmuhammadvirk@users.noreply.github.com>
2025-02-03 18:29:43 +0500

    fix issue realted to migrations

--- apis/transactions/views.py
+++ apis/transactions/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, FormView
 
 from apis.accounts.models import Account
-from apis.transactions.forms import MakeTransactionForm
+# from apis.transactions.forms import MakeTransactionForm
 
 from apis.transactions.utils import validate_and_process_transaction
 from apis.transactions.models import Transaction
@@ -21,29 +21,29 @@ class TransactionListView(LoginRequiredMixin, ListView):
         user_account_data = Account.objects.get(account_number=account_number)
         all_user_transaction = Transaction.objects.filter(account=user_account_data)
         return all_user_transaction
-
-
-class TransactionCreateView(LoginRequiredMixin, FormView):
-    template_name = 'transactions/make_transaction.html'
-    form_class = MakeTransactionForm
-    login_url = 'login/'
-    redirect_field_name = 'next'
-
-    def form_valid(self, form):
-        context = dict()
-        account_number = self.kwargs.get('account_number')
-
-        amount_user_entered = int(form.cleaned_data['amount'])
-        transaction_type_user_select = form.cleaned_data['transaction_type']
-
-        is_transaction_successful = validate_and_process_transaction(
-            transaction_type_user_select,
-            amount_user_entered,
-            account_number)
-
-        if is_transaction_successful:
-            context['messages'] = ['Transaction Successful']
-        else:
-            context['messages'] = ['Please try again something went wrong']
-
-        return render(form, self.template_name, context)
+#
+#
+# class TransactionCreateView(LoginRequiredMixin, FormView):
+#     template_name = 'transactions/make_transaction.html'
+#     form_class = MakeTransactionForm
+#     login_url = 'login/'
+#     redirect_field_name = 'next'
+#
+#     def form_valid(self, form):
+#         context = dict()
+#         account_number = self.kwargs.get('account_number')
+#
+#         amount_user_entered = int(form.cleaned_data['amount'])
+#         transaction_type_user_select = form.cleaned_data['transaction_type']
+#
+#         is_transaction_successful = validate_and_process_transaction(
+#             transaction_type_user_select,
+#             amount_user_entered,
+#             account_number)
+#
+#         if is_transaction_successful:
+#             context['messages'] = ['Transaction Successful']
+#         else:
+#             context['messages'] = ['Please try again something went wrong']
+#
+#         return render(form, self.template_name, context)
