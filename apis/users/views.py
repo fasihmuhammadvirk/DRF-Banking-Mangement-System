716be05a97a4a714c65d04234629c9b61247d7fa
Fasih Muhammad Virk <82199241+fasihmuhammadvirk@users.noreply.github.com>
2025-02-03 16:55:17 +0500

    Implemented crud api logic

--- apis/users/views.py
+++ apis/users/views.py
@@ -1,40 +1,13 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout
-from django.views.generic import View, RedirectView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
-
+from rest_framework import generics
 from apis.users.models import User
+from apis.users.serializers import UserSerializer
 
 
-class LoginPageView(LoginView):
-    template_name = 'user/login_page.html'
-    redirect_authenticated_user = True
-    next_page = '/'
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class UserSignupView(CreateView):
-    model = User
-    template_name = 'user/signup_page.html'
-    fields = ['username', 'password', 'address', 'phone_number']
-    success_url = '/'
-
-
-class MyLogoutView(RedirectView):
-    def get(self, request):
-        logout(request)
-        return redirect('login')
-
-
-class DashboardView(LoginRequiredMixin, View):
-    template_name = 'user/dashboard.html'
-    login_url = 'login/'
-    redirect_field_name = 'next'
-
-    def get(self, request):
-        current_user = request.user
-        context = {
-            'username': current_user.username.upper()
-        }
-
-        return render(request, self.template_name, context)
+class UserRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
