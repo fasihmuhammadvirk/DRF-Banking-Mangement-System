b686fac9c8a2a4233b5d8fb26d437d173d52fe92
Fasih Muhammad Virk <82199241+fasihmuhammadvirk@users.noreply.github.com>
2025-02-03 18:37:52 +0500

    updated url.py

--- apis/accounts/urls.py
+++ apis/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from apis.accounts.views import AccountListCreateView, AccountRetrieveUpdateDeleteView
 
 urlpatterns = [
-    path('api/v1/', AccountListCreateView.as_view(), name='account-list-create'),
-    path('api/v1/<int:pk>/', AccountRetrieveUpdateDeleteView.as_view(), name='account-detail'),
+    path('api/', AccountListCreateView.as_view(), name='account-list-create'),
+    path('api/<int:pk>/', AccountRetrieveUpdateDeleteView.as_view(), name='account-detail'),
 
 ]
