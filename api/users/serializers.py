3705a1ee73342f5e01aac2cacdf8a89e914e4eb4
Fasih Muhammad Virk <82199241+fasihmuhammadvirk@users.noreply.github.com>
2025-02-03 13:12:35 +0500

    implemented crud api for user

---
+++ api/users/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from api.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'address', 'phone_number']
